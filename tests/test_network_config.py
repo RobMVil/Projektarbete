2b9a9bbe6c1ae205d1628f9db0919289289d96f7
RobMVil <robert.vilcans@gmail.com>
2025-03-04 10:00:11 +0100

    Added test functions

--- tests/test_network_config.py
+++ tests/test_network_config.py
@@ -8,7 +8,27 @@ class Test_network_config:
         self.manager.update_hostname("1")
         self.manager.update_interface_state("down")
         self.manager.update_response_prefix("Standard Response")
+
     def teardown_method(self):
         self.manager.disconnect()
-    def test_show_host_name(self):
+
+    def test_show_hostname(self):
         assert self.manager.show_hostname() == "hostname: 1"
+
+    def test_show_interface_state(self):
+        assert self.manager.show_interface_state() == "interface_state: down"
+
+    def test_show_respnse_prefix(self):
+        assert self.manager.show_response_prefix() == "response_prefix: Standard Response"
+
+    def test_updated_hostname(self):
+        self.manager.update_hostname("2")
+        assert self.manager.show_hostname() == "hostname: 2"
+
+    def test_updated_interface_state(self):
+        self.manager.update_interface_state("up")
+        assert self.manager.show_interface_state() == "interface_state: up"
+
+    def test_updated_response_prefix(self):
+        self.manager.update_response_prefix("Non-Standard Response")
+        assert self.manager.show_response_prefix() == "response_prefix: Non-Standard Response"
