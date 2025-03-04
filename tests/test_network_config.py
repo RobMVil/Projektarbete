713df887b3a3ec2c4447db7bc9e8acb1efca9d82
RobMVil <robert.vilcans@gmail.com>
2025-03-04 08:59:59 +0100

    Added setup and teardown

--- tests/test_network_config.py
+++ tests/test_network_config.py
@@ -3,6 +3,10 @@ from network_config_manager import NetworkConfigManager
 
 class Test_network_config:
     def setup_method(self):
-        pass
+        self.manager = NetworkConfigManager()
+        self.manager.connect()
+        self.manager.update_hostname("1")
+        self.manager.update_interface_state("down")
+        self.manager.update_response_prefix("Standard Response")
     def teardown_method(self):
-        pass
+        self.manager.disconnect()
